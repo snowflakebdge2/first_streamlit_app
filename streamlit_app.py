073540c96f4b2dcb483b67656469a9e2bb7eb13e
snowflakebdge2 <121606707+snowflakebdge2@users.noreply.github.com>
2022-12-29 15:21:19 +0530

    Update streamlit_app.py
    Added sub heading
    Added breakfast menu text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit 
 
 streamlit.title('My parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Oatmeal')
+streamlit.text('Smoothie')
+streamlit.text('Egg')
